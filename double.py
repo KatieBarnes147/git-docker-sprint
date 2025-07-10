32acc627a650836d674fafe28854d899fc24ca48
Katie <barneska@merrimack.edu>
2025-07-10 07:12:27 +0000

    Feature: Merge feature branch with double.py

---
+++ double.py
@@ -0,0 +1,8 @@
+# double.py
+def double(num):
+    return num * 2
+
+if __name__ == "__main__":
+    print("Double 4 is:", double(4))
+# This code defines a function to double a number and prints the result for 4.
+# It is a simple demonstration of function definition and usage in Python.
